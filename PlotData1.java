e0e6e2f49d8bfe9812317b6de108383adfc39ad1
PseudoSyntax <43308680+PseudoSyntax@users.noreply.github.com>
2021-07-07 18:50:01 -0700

    Most Recent Code Updated
    Final Project version 
    - Updated name from main to PlotData
    - Added addData tab
    - Added PlotData tab
    - Added New Imports

--- PlotData1.java
+++ PlotData1.java
@@ -1,5 +1,6 @@
-/* CSE 360: Group Assignment
-        */
+// CSE 360: Group Assignment
+// Group Members: Imaan Ahmed, May Lister, Cera Monson, Italo Pennella
+ 
 import java.awt.*;
 import javax.swing.*;
 import java.awt.event.*;
@@ -8,21 +9,96 @@ import java.awt.GridLayout;
 import java.io.*;
 import java.net.MalformedURLException;
 import java.util.StringJoiner;
-import java.util.Vector;
+
 import javax.swing.SwingUtilities;
+
+import java.awt.Color;
+import java.io.BufferedReader;
+
+import java.io.FileReader;
+import java.io.IOException;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+import java.util.Date;
+
+
+import javax.swing.JFrame;  
+ 
+import javax.swing.WindowConstants;  
+import org.jfree.chart.ChartFactory;  
+import org.jfree.chart.ChartPanel;  
+import org.jfree.chart.JFreeChart;  
+import org.jfree.chart.plot.XYPlot;
+import org.jfree.data.time.Day;
+import org.jfree.data.time.TimeSeries;
+import org.jfree.data.time.TimeSeriesCollection;
+import org.jfree.data.xy.XYDataset; 
+
+
 
 import java.net.URL;
 
-public class Main {
+public class PlotData extends JFrame 
+{
+	
+	//===============================================PLOT DATA PANEL===============================================
+	
+	public PlotData(String title) 
+	{  
+	    super(title);  
+	  
+	    // Create dataset  
+	    XYDataset dataset = createDataset();  
+	  
+	    // Create chart  
+	    JFreeChart chart = ChartFactory.createTimeSeriesChart(   
+	        "Temperature vs. Date Across Various Cities",   
+	        "Date", "Temperature", dataset);  
+	  
+	      
+	    //Changes background color  
+	    XYPlot plot = (XYPlot)chart.getPlot();  
+	    plot.setBackgroundPaint(Color.LIGHT_GRAY);  
+	      
+	     
+	    // Create Panel  
+	    ChartPanel panel = new ChartPanel(chart);  
+	    setContentPane(panel);  
+	} 
+	
+	static String data [][] = {{"Something", "9/21/2021", "90", "F"}};
+	static String column[]={"City Name","Date","Temperature","Type"};
+	
+	private static XYDataset createDataset() {
+		TimeSeriesCollection dataset = new TimeSeriesCollection(); 
+		
+		TimeSeries series1 = new TimeSeries("Cities");
+		
+		try
+		{
+			for (int i = 0; i < data.length; i++)
+			{
 
-    static String data [][] = { {"Phoenix","7/2","100","F"},
-            {"Test","7/3","39","C"},
-            {"Random","5/6","70","F"}};
-    static String column[]={"City Name","Date","Temperature","Type"};
+				TimeSeries series2 = new TimeSeries("Phoenix");
+				SimpleDateFormat simpleDateFormat = new SimpleDateFormat("M/d/yyyy");
+				String date = simpleDateFormat.format(new Date());
+				Date d = simpleDateFormat.parse(data[i][1]);
+				series1.add(new Day(d), Double.parseDouble(data[i][2]));
 
-    public static void main(String[] args) throws InterruptedException, MalformedURLException, ClassNotFoundException, UnsupportedLookAndFeelException, InstantiationException, IllegalAccessException
-    {
-        //UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
+			}
+		} catch (ParseException e)
+		{
+			e.printStackTrace();
+		}
+	    dataset.addSeries(series1);
+	    return dataset;
+	  }  
+	
+	
+	public static void main(String[] args) throws InterruptedException, MalformedURLException, ClassNotFoundException, UnsupportedLookAndFeelException, InstantiationException, IllegalAccessException 
+	{
         UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");  //Theme for the Windows, feel free to change it back to windows
 
         JProgressBar progressBar; //Progress bar variable
@@ -42,8 +118,6 @@ public class Main {
         splashScreen.setLocationRelativeTo(null);  //Sets in middle of user screen
 
         progressBar = new JProgressBar();
-        //progressBar.setStringPainted(true);  //This will show the percentage but is forced disabled with nimbus theme
-        //progressBar.setForeground(Color.ORANGE);  //Color is forced default orange when using nimbus theme
         progressBar.setMaximum(100);
         progressBar.setMinimum(0);
 
@@ -51,6 +125,7 @@ public class Main {
         splashScreen.add(progressBar);
         progressBar.setBounds(-10, 300, 404, 14); //Bounds of Progress Bar
         progressBar.setPreferredSize(new Dimension(310, 30)); //Size of Progress BAr
+       
 
         //Runnable inner class allows thread to move along bar
         Thread thread = new Thread()
@@ -71,7 +146,7 @@ public class Main {
             }
         };
         thread.start();
-        Thread.sleep(100);  //Display Screen for a bit
+        Thread.sleep(5000);  //Display Screen for a bit
         splashScreen.setVisible(false); //Removes splashscreen load
         splashScreen.dispose();  //Deletes frame
         /******SPLASH SCREEN******/
@@ -94,6 +169,7 @@ public class Main {
         JPanel loadDataPanel = new JPanel();
         JPanel addDataPanel = new JPanel();
         JPanel saveDataPanel = new JPanel();
+        JPanel plotDataPanel = new JPanel();
 
 
         //===============================================HOME PANEL===============================================
@@ -104,19 +180,12 @@ public class Main {
         JScrollPane jsp = createTables(tempor);
         homePanel.add(jsp);
 
-
-
-
-
-
-
-
         //===============================================ABOUT PANEL===============================================
 
         //panel and color
         JPanel pain = new JPanel();
         pain.setBackground(Color.WHITE);
-
+        
         //creating text
         JLabel group = new JLabel("\t\tTeam #28\n\n");
         group.setFont(new Font("Times New Roman", Font.BOLD,18));
@@ -128,112 +197,113 @@ public class Main {
         name3.setFont(new Font("Times New Roman", Font.PLAIN,15));
         JLabel name4 = new JLabel("\t\t\tItalo Pennella");
         name4.setFont(new Font("Times New Roman", Font.PLAIN,15));
-
+        
         //setting layout and adding text
         BoxLayout Box = new BoxLayout(pain, BoxLayout.Y_AXIS);
         pain.setLayout(Box);
-
+        
         pain.add(group);
         pain.add(name1);
         pain.add(name2);
         pain.add(name3);
         pain.add(name4);
-
+        
         //adding panel to tabs
         tabs.addTab("About", pain);
-
+        
         //===============================================LOAD PANEL===============================================
 
         //add load tab
         tabs.addTab("Load Data", loadDataPanel);
-
+        
         //setting up text field for user to enter filename
         JLabel selectFile = new JLabel("Select File");
         loadDataPanel.add(selectFile);
         JTextField fileInput = new JTextField(10);
         loadDataPanel.add(fileInput);
-
+        
         //select file button
         JButton select = new JButton();
         select.setSize(400,100);
         select.setText("Select File");
         select.setVisible(true);
         loadDataPanel.add(select);
-
+        
         //result of pushing button
         select.addActionListener(new ActionListener()
-        {
-            public void actionPerformed(ActionEvent e)
-            {
-                String file = fileInput.getText();
-                try
-                {
-                    //file reader
-                    FileReader fr = new FileReader(file);
-                    BufferedReader br = new BufferedReader(fr);
-                    String line = br.readLine();
-                    line = br.readLine();
-
-                    //counting amount of data for new array
-                    int count = 0;
-
-                    while(line != null)
-                    {
-                        line = br.readLine();
-                        count++;
-                    }
-
-                    //second file reader to
-                    FileReader fr2 = new FileReader(file);
-                    BufferedReader br2 = new BufferedReader(fr2);
-                    String line2 = br2.readLine();
-
-                    line2 = br2.readLine();						//to skip over column names
-                    String[][] data2 = new String[count][4];	//creating new array
-                    int i = 0;
-
-                    //read and add data to new array
-                    while (line2 != null)
-                    {
-                        String[] words = line2.split(",", 4);
-                        data2[i][0] = words[0];
-                        data2[i][1] = words[1];
-                        data2[i][2] = words[2];
-                        data2[i][3] = words[3];
-
-                        i++;
-                        line2 = br2.readLine();
-                    }
-
-                    //replace data
-                    changer(data2);
-
-                    //close readers
-                    br.close();
-                    br2.close();
-
-                }
-                catch (IOException e2)
-                {
-                    System.out.println("IO Exception");
-                }
-
-                //update home panel with new data
-                homePanel.removeAll();
-                homePanel.add(createTables(tempor));
-            }
-        });
+        	{
+        		public void actionPerformed(ActionEvent e)
+        		{
+        			String file = fileInput.getText();
+        			try
+        			{
+        				//file reader
+        				FileReader fr = new FileReader(file);
+        				BufferedReader br = new BufferedReader(fr);
+        				String line = br.readLine();
+        				line = br.readLine();
+        				
+        				//counting amount of data for new array
+        				int count = 0;
+        				
+        				while(line != null)
+        				{
+        					line = br.readLine();
+        					count++;
+        				}
+        				
+        				//second file reader to
+        				FileReader fr2 = new FileReader(file);
+        				BufferedReader br2 = new BufferedReader(fr2);
+        				String line2 = br2.readLine();
+        				
+        				line2 = br2.readLine();						//to skip over column names
+        				String[][] data2 = new String[count][4];	//creating new array
+        				int i = 0;
+        				
+        				//read and add data to new array
+        				while (line2 != null)
+        				{
+        					String[] words = line2.split(",", 4);
+        					data2[i][0] = words[0];
+        					data2[i][1] = words[1];
+        					data2[i][2] = words[2];
+        					data2[i][3] = words[3];
+        					
+        					i++;
+        					line2 = br2.readLine();
+        				}
+        				
+        				//replace data
+        				changer(data2);
+        				
+        				//close readers
+        				br.close();
+        				br2.close();
+        				
+        			}
+        			catch (IOException e2)
+        			{
+        				System.out.println("IO Exception");
+        			}
+        			
+        			//update home panel with new data
+        			homePanel.removeAll();
+        			homePanel.add(createTables(tempor));
+        		}
+        	});
 
 
         //===============================================ADD PANEL===============================================
 
-
+       
         tabs.addTab("Add Data", addDataPanel);
+
 
         JTable jt_add =new JTable(data,column);
         //jt.setBounds(30,40,200,300);
         JScrollPane sp2=new JScrollPane(jt_add);
-        addDataPanel.add(sp2);
+
 
 
         JLabel cityName = new JLabel("City");
@@ -264,21 +334,19 @@ public class Main {
         JButton addRowBtn = new JButton("Submit");
         addDataPanel.add(addRowBtn);
 
-        //String cityInput = cityText.getText();
-        //String dateInput = dateText.getText();
-        //String tempInput = tempText.getText();
-        //String degreeInput = degreeText.getText();
+        String cityInput = cityText.getText();
 
-
-
+        //final int[] count = {1};
+        final int[] tail = {0};
         addRowBtn.addActionListener(new ActionListener() {
-            int count=1;//helps array grow with each btn click
-            //int tail = -1;
+            //int count=1;//helps array grow with each btn click
+
             int i,j;
 
+            String temp = cityText.getText();
             public void actionPerformed(ActionEvent e) {
-                System.out.println("length of row "+data.length);
-                System.out.println("length of col "+data[0].length);
+                int count = 1;
+
                 String cityInput = cityText.getText();
                 String dateInput = dateText.getText();
                 String tempInput = tempText.getText();
@@ -286,30 +354,36 @@ public class Main {
 
                 int n = data.length;//row size
                 int m = data[0].length;//col size
+                int index=0;
 
-                String[][] data2 = new String[count+data.length][4]; //new array declared
-                for ( i=0; i < data.length;i++ ) {
+
+
+                String[][] data2 = new String[count +data.length][4]; //new array declared
+                for ( i = 0; i < data.length;i++ ) {
                     for ( j=0; j < data[0].length;j++) { //we used num[0] because we need the length of the rows not the columns
                         data2[i][j] = data[i][j];
-                        System.out.println(data2[i][j]); //Copies array
+                        
                     }
                 }
-                for ( i = data.length; i < data.length+count;i++ ) {
+                for (i = data.length; i < data.length+ count; i++ ) {//growing
                     for (j = 0; j < 4; j++) {
-                        //while(tail!=count) {
-                        data2[i][0] = cityInput;
-                        data2[i][1] = dateInput;
-                        data2[i][2] = tempInput;
-                        data2[i][3] = degreeInput;
-                        //tail++;
-                        //}
-                        System.out.println(data2[i][j]); //Copies array
+
+                                    data2[i][0] = cityInput;
+                                    data2[i][1] = dateInput;
+                                    data2[i][2] = tempInput;
+                                    data2[i][3] = degreeInput;
+                                   
+                       
+                        
                     }
                 }
-                //data[data2.length][4] = data2[i][j];//update data array size with data2 array size
+
                 count++;//updates data length
-                System.out.println("count var: "+count);
-            }
+               
+                changer(data2);
+                homePanel.removeAll();
+                homePanel.add(createTables(tempor));
+            }//end of actionlistener
 
         });
 
@@ -343,12 +417,12 @@ public class Main {
                 String fileName = userInput.getText();
 
                 //writing data to new file
-                try (BufferedWriter bw = new BufferedWriter(new FileWriter(new File(fileName +".csv"))))
+                try (BufferedWriter bw = new BufferedWriter(new FileWriter(new File(fileName +".csv")))) 
                 {
                     StringJoiner joiner = new StringJoiner(",");
 
                     //writing column names to file
-                    for (int col = 0; col < column.length; col++)
+                    for (int col = 0; col < column.length; col++) 
                     {
                         joiner.add(column[col]);
                     }
@@ -359,32 +433,26 @@ public class Main {
                     //writing data to file
                     for(int r = 0; r<data.length; r++)
                     {
-                        joiner = new StringJoiner(",");
-                        for (int c = 0; c<data[r].length; c++)
-                        {
-                            Object obj = data[r][c];
-                            String value = obj == null ? "null" : obj.toString();
-                            joiner.add(value);
-                        }
-                        bw.write(joiner.toString());
+                    	joiner = new StringJoiner(",");
+                    	for (int c = 0; c<data[r].length; c++)
+                    	{
+                    		Object obj = data[r][c];
+                    		String value = obj == null ? "null" : obj.toString();
+                    		joiner.add(value);
+                    	}
+                    	bw.write(joiner.toString());
                         bw.newLine();
                     }
-
-                }
-                catch (IOException exp)
+                    
+                } 
+                catch (IOException exp) 
                 {
                     exp.printStackTrace();
-                }
-
+                }  
             }
         });
 
         //Add JPanel for tables
-        JPanel allTables = new JPanel(new GridLayout(6, 1));
-        allTables.add(new JLabel("Table Layout"));
-
-
-        JScrollPane plotDataPanel = new JScrollPane(allTables);
         tabs.addTab("Plot Data", plotDataPanel);
 
         //baseFrame layout
@@ -392,46 +460,50 @@ public class Main {
         baseFrame.add(tabs, BorderLayout.CENTER);
         baseFrame.setVisible(true);
         baseFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//stop when closed
+        
+      //===============================================PLOT DATA PANEL===============================================
+        JButton plot = new JButton();
+        plot.setSize(400,400);
+        plot.setText("Plot data");
+        plot.setVisible(true);
+        plotDataPanel.add(plot);
 
-
-        /****TERMS OF USE FRAME****/
-        /*
-        JFrame frame = new JFrame();
-        frame.setLayout(new FlowLayout());
-        frame.add(new JLabel("Terms of Use"));
-        frame.setSize(400,450);
-        JLabel README = new JLabel("You agree to give us A+");
-        JButton AgreeBtn = new JButton("I AGREE");
-        JButton DisagreeBtn = new JButton("I DISAGREE FOR NOW");
-        frame.setLocationRelativeTo(null);
-        frame.setResizable(false);
-        frame.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
-        TextArea Terms = new TextArea(20,50);
-        Terms.append("Text goes here");
-        Terms.setEditable(false);
-        JScrollPane scroll = new JScrollPane(Terms);
-        Terms.setBackground(Color.LIGHT_GRAY);
-        frame.add(scroll);
-        frame.add(AgreeBtn);
-        frame.add(DisagreeBtn);
-        frame.add(README);
-        frame.setVisible(true);
-        //JOptionPane.showMessageDialog(frame,"Warning.","Warning Box", JOptionPane.WARNING_MESSAGE);
-        //custom title, custom icon
-*/
-    }
-
-    public static void changer(String[][] temps)
-    {
-        data = temps;
-    }
-
-    public static JScrollPane createTables(JTable jazz)
-    {
-        jazz = new JTable(data,column);
+        //action listener for button
+        plot.addActionListener(new ActionListener()
+        {
+        	
+            public void actionPerformed(ActionEvent e)
+            { 
+            	
+            	XYDataset dataset = createDataset();  
+            	
+            	 SwingUtilities.invokeLater(() -> {  
+       		      PlotData example = new PlotData("Time Series Chart");  
+       		      example.setSize(1000, 500);  
+       		      example.setLocationRelativeTo(null);  
+       		      example.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);  
+       		      example.setVisible(true);  
+       		    });  
+            }
+            
+        });
+        
+        
+       
+	}
+	
+	
+	public static void changer(String[][] temps)
+	{
+		data = temps;
+	}
+	
+	public static JScrollPane createTables(JTable jazz)
+	{
+		jazz = new JTable(data,column);
         JScrollPane sp=new JScrollPane(jazz);
         return sp;
-    }
+	}
 
-
+    
 }//end of Main
